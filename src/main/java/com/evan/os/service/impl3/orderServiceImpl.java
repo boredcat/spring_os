622db71882fd0c491655c22077491e4e445a589f
EvanZhao <phzhao7548@gmail.com>
2019-06-29 20:16:39 +0800

    拦截器事务处理

---
+++ src/main/java/com/evan/os/service/impl3/orderServiceImpl.java
@@ -0,0 +1,35 @@
+package com.evan.os.service.impl3;
+
+import com.evan.os.dao.OrderDao;
+import com.evan.os.dao.ProductDao;
+import com.evan.os.entity.Order;
+import com.evan.os.entity.Product;
+import com.evan.os.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.Date;
+
+@Service
+public class orderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @Autowired
+    private ProductDao productDao;
+
+
+    public void addOrder(final Order order) {
+        order.setCreateTime(new Date());
+        order.setStatus("代付款");
+
+        orderDao.insert(order);
+        Product product = productDao.select(order.getProductsId());
+        product.setStock(product.getStock()-order.getNumber());
+        productDao.update(product);
+    }
+}
